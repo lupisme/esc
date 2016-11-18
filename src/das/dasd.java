2f694b48d3b5b3b2933159df3b2d33b98156bd5a
lupisme <2945362198@qq.com>
2016-11-18 21:53:08 +0800

    123

--- src/das/dasd.java
+++ src/das/dasd.java
@@ -2,7 +2,8 @@ package das;
 
 public class dasd {
 	public static void main(String[] args){
-		System.out.println("hah123a");
-		
+		System.out.println("ah");
+		System.out.println("ah");
+        System.out.println("ah");
     }
 }
